7cc7d4aae14fd9dfd5fac5aff954c0d3b90071b2
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-27 15:16:54 -0400

    Version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,5 @@
 - Added `get_count` method to obtain refcount of `Arc`, `ArcBorrow`, `ThinArc`, and `OffsetArc`
 - Added `Hash` implementation to `ArcBorrow`, `ThinArc`, and `OffsetArc`, along with `HeaderSlice` and `HeaderWithLength`
 - Added `Ord` implementation to `HeaderSlice`
+- Changed default `Arc` implementation to `OffsetArc`, renaming the old `triomphe::Arc` to `ArcHandle`
+- Expanded the API for `ArcBorrow` and the new `Arc`
--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,7 @@
 
 //! [![crates.io](https://img.shields.io/crates/v/elysees)](https://crates.io/crates/elysees)
 //! [![Downloads](https://img.shields.io/crates/d/elysees)](https://crates.io/crates/elysees)
-//! Fork of Arc. This has the following advantages over std::sync::Arc:
-//!
-//! * `elysees::Arc` doesn't support weak references: we save space by excluding the weak reference count, and we don't do extra read-modify-update operations to handle the possibility of weak references.
-//! * `elysees::UniqueArc` allows one to construct a temporarily-mutable `Arc` which can be converted to a regular `elysees::Arc` later
-//! * `elysees::OffsetArc` can be used transparently from C++ code and is compatible with (and can be converted to/from) `elysees::Arc`
-//! * `elysees::ArcBorrow` is functionally similar to `&elysees::Arc<T>`, however in memory it's simply `&T`. This makes it more flexible for FFI; the source of the borrow need not be an `Arc` pinned on the stack (and can instead be a pointer from C++, or an `OffsetArc`). Additionally, this helps avoid pointer-chasing.
-//! * `elysees::Arc` has can be constructed for dynamically-sized types via `from_header_and_iter`
-//! * `elysees::ThinArc` provides thin-pointer `Arc`s to dynamically sized types
-//! * `elysees::ArcUnion` is union of two `elysees:Arc`s which fits inside one word of memory
-//!
-//!  This currently just adds a getter to the refcount, but some other additional features are planned, including 3 and 4-variant `ArcUnion`s, and `ArcUnion`s with `ThinArc`s.
+//! Fork of Arc, now with more pointer tricks.
 
 #![allow(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
