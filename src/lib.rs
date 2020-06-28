3324a6e83bc975d8118457d16442e5f5963b377f
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-28 00:51:14 -0400

    Readme, badges

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@
 //! [![crates.io](https://img.shields.io/crates/v/elysees)](https://crates.io/crates/elysees)
 //! [![Downloads](https://img.shields.io/crates/d/elysees)](https://crates.io/crates/elysees)
 //! [![Documentation](https://docs.rs/elysees/badge.svg)](https://docs.rs/elysees/)
+//! [![Pipeline status](https://gitlab.com/rain-lang/elysees/badges/master/pipeline.svg)](https://gitlab.com/rain-lang/elysees)
+//! [![codecov](https://codecov.io/gl/rain-lang/elysees/branch/master/graph/badge.svg)](https://codecov.io/gl/rain-lang/elysees)
+//! [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 //! 
 //! Fork of Arc, now with more pointer tricks.
 
