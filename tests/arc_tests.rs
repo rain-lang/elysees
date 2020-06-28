41b15d757726df2f93f380914813ff3a5b2da798
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-27 22:35:34 -0400

    Test

--- tests/arc_tests.rs
+++ tests/arc_tests.rs
@@ -25,4 +25,14 @@ fn basic_arc_creation_works() {
     let y = y.shareable();
     assert!(y.is_unique());
     assert_eq!(*y.deref(), 80);
+
+    let yb = y.borrow_arc();
+    assert_eq!(*yb.deref(), 80);
+    assert_eq!(ArcBorrow::count(yb, Ordering::Relaxed), 1);
+    let yb2 = yb.clone();
+    assert_eq!(ArcBorrow::count(yb, Ordering::Relaxed), 1);
+    assert_eq!(ArcBorrow::count(yb2, Ordering::Relaxed), 1);
+    let ybr = ArcBorrow::as_arc(&yb2);
+    assert_eq!(Arc::count(ybr, Ordering::Relaxed), 1);
+    assert!(ybr.is_unique());
 }
