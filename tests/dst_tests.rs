d126a15d88ccdc6169727b988d36a7de165eb4ce
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-06 19:58:52 -0400

    Dst

--- tests/dst_tests.rs
+++ tests/dst_tests.rs
@@ -91,6 +91,7 @@ fn dst_union_test() {
     assert!(union4.d().is_some());
 }
 
+#[cfg(feature = "slice-dst")]
 #[test]
 fn failed_dst_alloc() {
     fn fallible_alloc(_: NonNull<SliceWithHeader<&str, u8>>) -> Result<(), &str> {
